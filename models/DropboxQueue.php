b23242995e13471b7ad2ec010142f1620017fddc
Florian Bieringer <florian.bieringer@uni-passau.de>
2015-09-22 14:10:25 +0200

    Dont stop thread on dropbox connection fail

--- models/DropboxQueue.php
+++ models/DropboxQueue.php
@@ -35,10 +35,13 @@ class DropboxQueue extends SimpleORMap {
         // If file doesnt exists on dropbox or is older on dropbox
         if (!$metadata || $metadata['modified'] < $this->date) {
             $f = fopen($this->filepath, "rb");
-            $result = $Client->uploadFile($this->dropboxpath, Dropbox\WriteMode::force(), $f);
+            try {
+                $result = $Client->uploadFile($this->dropboxpath, Dropbox\WriteMode::force(), $f);
+                $this->delete();
+            } catch (Dropbox\Exception_BadResponseCode $e) {
+            }
             fclose($f);
         }
-        $this->delete();
     }
 
 }
